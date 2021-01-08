852c1927ccd7fb864c33a196ced9b89be5590e49
John Zhou <shanghaizmy@yeah.net>
2021-01-08 10:46:34 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/com/zmy/lie/LieApplication.java
@@ -0,0 +1,20 @@
+package com.zmy.lie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Learning is endless.
+ * 简称LIE（23333一语双关？
+ * 企图做一款学习视频网站！
+ * Go!
+ * @author John Zhou
+ * @date 2021/1/2 0:57
+ */
+@SpringBootApplication
+public class LieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LieApplication.class, args);
+	}
+
+}
